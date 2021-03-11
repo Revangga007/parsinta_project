c74aedc6154b6f4b6b70bc5fd4b6f1ebb2ecd85f
Revangga007 <revanggaajip2@gmail.com>
2021-03-11 18:01:28 +0700

    Mencoba data binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
 }
